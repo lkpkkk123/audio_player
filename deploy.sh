91ced7f2f18526c97df019552976f5195b184745
likunpeng <43669872@qq.com>
2025-12-22 15:58:35 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 rsync -avz media/ root@192.168.8.100:/opt/audio_player/media/
 rsync -avz public/ root@192.168.8.100:/opt/audio_player/public/
 rsync -avz server.py root@192.168.8.100:/opt/audio_player/server.py
+rsync -avz service/ root@192.168.8.100:/opt/audio_player/service/
